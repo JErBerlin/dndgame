b9dc74ee5abd3348e982ae03a287a514b5aa7a8c
JerBerlin <jeronir@hotmail.com>
2024-04-27 14:20:56 +0200

    correct approveaction function in game

--- internal/game_master/gamemaster.go
+++ internal/game_master/gamemaster.go
@@ -1,6 +1,12 @@
 // Package game_master presents the game master agent responsible for directing the game.
 package game_master
 
+import (
+	"errors"
+
+	"github.com/jerberlin/dndgame/internal/action"
+)
+
 // GameMasterStatus defines possible states of a game master.
 type GameMasterStatus int
 
@@ -16,9 +22,14 @@ type GameMaster struct {
 	Status GameMasterStatus
 }
 
-// ApproveAction approves an action instance chosen by a player to be executed by the players character.
-func (gm *GameMaster) ApproveAction(instanceID string) {
-	ai.Approved = true // For now, it simply approves the action.
+// ApproveAction finds an action instance by ID and sets its Approved status to true. Returns an error if not found.
+// TODO: decouple action instances from approve action function, f.ex. injecting the action manager service or with event-driven aproval.
+func (gm *GameMaster) ApproveAction(instances map[string]*action.ActionInstance, instanceID string) error {
+	if ai, exists := instances[instanceID]; exists {
+		ai.Approved = true
+		return nil
+	}
+	return errors.New("action instance not found")
 }
 
 // SetStatus changes the status of the game master.

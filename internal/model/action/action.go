1aef5e38c63e373da8e608de5bc9e4ff5172e575
JErBerlin <jeronir@hotmail.com>
2024-05-13 15:25:26 +0200

    add performed field in action instance; some renamings and style

--- internal/model/action/action.go
+++ internal/model/action/action.go
@@ -3,7 +3,7 @@ package action
 
 // Action represents a template for possible actions in the game.
 type Action struct {
-	ActionID   string
+	ActionId   string
 	Name       string
 	BaseXPCost int
 }
@@ -11,18 +11,19 @@ type Action struct {
 // ActionInstance represents a specific action taken by a character, customised to them and to a given scenario
 // The action will be chosen by the player of the character but has to be approved by the game master.
 type ActionInstance struct {
-	Action       Action
-	CharacterID  string
-	CustomXPCost int
-	Approved     bool
+    Action       Action
+    CharacterId  string
+    CustomXPCost int
+    Approved     bool
+    Performed    bool  // This field indicates whether the action has been performed.
 }
 
+
 // CreateInstance creates a new action instance customized for a character.
-func (a *Action) CreateInstance(characterID string, customXPCost int) ActionInstance {
+func (a *Action) CreateInstance(characterId string, customXPCost int) ActionInstance {
 	return ActionInstance{
 		Action:       *a,
-		CharacterID:  characterID,
+		CharacterId:  characterId,
 		CustomXPCost: customXPCost,
-		Approved:     false,
 	}
 }
--- internal/model/action/action_test.go
+++ internal/model/action/action_test.go
@@ -6,25 +6,25 @@ import (
 
 func TestActionCreation(t *testing.T) {
 	action := Action{
-		ActionID:   "a1",
+		ActionId:   "a1",
 		Name:       "Fireball",
 		BaseXPCost: 50,
 	}
 
-	if action.ActionID != "a1" || action.Name != "Fireball" || action.BaseXPCost != 50 {
+	if action.ActionId != "a1" || action.Name != "Fireball" || action.BaseXPCost != 50 {
 		t.Errorf("Action creation failed, got: %+v", action)
 	}
 }
 
 func TestCreateInstance(t *testing.T) {
 	action := Action{
-		ActionID:   "a1",
+		ActionId:   "a1",
 		Name:       "Fireball",
 		BaseXPCost: 50,
 	}
 	instance := action.CreateInstance("char123", 60)
 
-	if instance.Action != action || instance.CharacterID != "char123" || instance.CustomXPCost != 60 || instance.Approved {
+	if instance.Action != action || instance.CharacterId != "char123" || instance.CustomXPCost != 60 || instance.Approved {
 		t.Errorf("CreateInstance did not properly initialize, got: %+v", instance)
 	}
 }
